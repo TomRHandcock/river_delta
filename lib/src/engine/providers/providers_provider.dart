f03c6cb9dedfc1557a3115239e7cdcbfbaa149a1
TomRHandcock <33254448+TomRHandcock@users.noreply.github.com>
2024-11-25 20:50:32 +0000

    refactor: Update `ProvidersProvider`
    Update `ProvidersProvider` to allow for incoming partial updates.

--- lib/src/engine/providers/providers_provider.dart
+++ lib/src/engine/providers/providers_provider.dart
@@ -1,4 +1,6 @@
+import 'package:collection/collection.dart';
 import 'package:river_delta/src/engine/providers/extension_receiver.dart';
+import 'package:river_delta/src/ui/graph/graph_render_object.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../observer/dto_models.dart';
@@ -6,19 +8,35 @@ import '../observer/dto_models.dart';
 part 'providers_provider.g.dart';
 
 @riverpod
-List<ProviderDto> providersProvider(ProvidersProviderRef ref) {
-  ref.watch(vmServiceProvider.future).then((vmService) {
-    vmService.onExtensionEvent.listen((event) {
-      switch(event.extensionKind) {
+class ProvidersProvider extends _$ProvidersProvider {
+  List<ProviderDto> _providers = List.empty();
+
+  @override
+  Future<List<ProviderDto>> build() async {
+    final eventStream = await ref.watch(vmServiceProvider.future);
+    final subscription = eventStream.onExtensionEvent.listen((event) {
+      switch (event.extensionKind) {
+        case "ext.river_delta.add":
+          final provider = ProviderDto.fromJson(event.extensionData!.data);
+          _providers = _providers + [provider];
         case "ext.river_delta.update":
-          ref.state = ProviderListDto.fromJson(event.extensionData!.data).providers;
-        case "ext.river_delta.addProvider":
-          ref.state = ref.state + [ProviderDto.fromJson(event.extensionData!.data)];
-        case "ext.river_delta.removeProvider":
-          final removed = ProviderDto.fromJson(event.extensionData!.data).name;
-          ref.state = ref.state.where((incumbent) => incumbent.name != removed).toList();
+          final provider = ProviderDto.fromJson(event.extensionData!.data);
+          _providers = _providers
+              .whereNot((it) =>
+                  it.name == provider.name && it.argument == provider.argument)
+              .toList() + [provider];
+        case "ext.river_delta.dispose":
+          final provider = ProviderDto.fromJson(event.extensionData!.data);
+          _providers = _providers
+              .whereNot((it) =>
+          it.name == provider.name && it.argument == provider.argument)
+              .toList();
       }
+      ref.notifyListeners();
     });
-  });
-  return [];
+    ref.onDispose(() {
+      subscription.cancel();
+    });
+    return _providers;
+  }
 }
--- lib/src/engine/providers/providers_provider.g.dart
+++ lib/src/engine/providers/providers_provider.g.dart
@@ -6,13 +6,13 @@ part of 'providers_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$providersProviderHash() => r'06b14ced105655589a4a6a6b930364fd03bf2990';
+String _$providersProviderHash() => r'fc5fabd54effb549414fb401c13a9d6a1abd27df';
 
-/// See also [providersProvider].
-@ProviderFor(providersProvider)
-final providersProviderProvider =
-    AutoDisposeProvider<List<ProviderDto>>.internal(
-  providersProvider,
+/// See also [ProvidersProvider].
+@ProviderFor(ProvidersProvider)
+final providersProviderProvider = AutoDisposeAsyncNotifierProvider<
+    ProvidersProvider, List<ProviderDto>>.internal(
+  ProvidersProvider.new,
   name: r'providersProviderProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
@@ -21,6 +21,6 @@ final providersProviderProvider =
   allTransitiveDependencies: null,
 );
 
-typedef ProvidersProviderRef = AutoDisposeProviderRef<List<ProviderDto>>;
+typedef _$ProvidersProvider = AutoDisposeAsyncNotifier<List<ProviderDto>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
