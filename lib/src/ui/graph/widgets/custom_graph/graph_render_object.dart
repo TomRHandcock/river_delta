3dcb476c5d3a9eb209b3d72dcaf75d2f90c9079e
TomRHandcock <33254448+TomRHandcock@users.noreply.github.com>
2025-02-21 22:01:21 +0000

    refactor: Refactor layout of `CustomGraphWidget`
    Refactors `CustomGraphWidget` to base it's size on the sizes of it's children.

--- lib/src/ui/graph/widgets/custom_graph/graph_render_object.dart
+++ lib/src/ui/graph/widgets/custom_graph/graph_render_object.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:collection/collection.dart';
 import 'package:devtools_app_shared/ui.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +8,10 @@ import 'package:river_delta/src/engine/utils/utils.dart';
 import 'package:river_delta/src/ui/graph/viewmodel/graph_state.dart';
 
 class CustomGraphWidgetParentData extends ContainerBoxParentData<RenderBox> {
-  GraphNode? node;
+  late GraphNode node;
+  ({int y, int x})? position;
+  Size? size;
+  Rect? rect;
 
   CustomGraphWidgetParentData();
 }
@@ -15,10 +20,12 @@ class RenderCustomGraphWidget extends RenderBox
     with
         ContainerRenderObjectMixin<RenderBox, CustomGraphWidgetParentData>,
         DebugOverflowIndicatorMixin {
+  static const _padding = EdgeInsets.all(100);
+  static const _verticalSpacing = 100.0;
+  static const _horizontalSpacing = 50.0;
 
   GraphState _graph;
   late List<List<GraphNode>> _layeredTree;
-  Map<GraphNode, Rect> _cachedChildRects = {};
 
   RenderCustomGraphWidget({required GraphState graph}) : _graph = graph {
     _layeredTree = _buildLayeredTree(graph);
@@ -46,11 +53,19 @@ class RenderCustomGraphWidget extends RenderBox
     });
   }
 
-  Offset _getOffsetForGraphNode(
+  ({int y, int x}) _getPositionForNode(
       GraphNode node, Set<DeltaProvider> allProviders) {
     final y = node.distanceToRoot(allProviders, longest: true);
     final x = _layeredTree[y].indexOf(node);
-    return Offset(x * 250, y * 120) + Offset(100, 100);
+    return (y: y, x: x);
+  }
+
+  Offset _getOffsetForGraphNode(
+      GraphNode node, Set<DeltaProvider> allProviders) {
+    final position = _getPositionForNode(node, allProviders);
+    return Offset(
+            position.x * _horizontalSpacing, position.y * _verticalSpacing) +
+        _padding.topLeft;
   }
 
   @override
@@ -69,11 +84,15 @@ class RenderCustomGraphWidget extends RenderBox
     bool hit = false;
     final globalPosition = position.translate(defaultSpacing, defaultSpacing);
     while (child != null) {
-      final childNode = child.parentData.asOrNull<CustomGraphWidgetParentData>()?.node;
-      if(childNode != null) {
-        final childPosition = _cachedChildRects[childNode]!.topLeft;
-        final positionOnChild = globalPosition.translate(-childPosition.dx, -childPosition.dy);
-        hit = hit || child.hitTest(result, position: positionOnChild);
+      final parentData =
+          child.parentData.asOrNull<CustomGraphWidgetParentData>();
+      final childRect = parentData?.rect;
+      if (childRect != null) {
+        final childPosition = childRect.topLeft;
+        final positionOnChild =
+            globalPosition.translate(-childPosition.dx, -childPosition.dy);
+        hit = hit ||
+            (child.hasSize && child.hitTest(result, position: positionOnChild));
       }
       child = childAfter(child);
     }
@@ -82,29 +101,82 @@ class RenderCustomGraphWidget extends RenderBox
 
   @override
   void performLayout() {
+    List<List<Size>> sizes = List.generate(
+      graph.depth + 1,
+      (yPos) => List.generate(_layeredTree[yPos].length, (_) => Size.zero),
+    );
     RenderBox? child = firstChild;
     while (child != null) {
-      child.layout(const BoxConstraints());
+      final parentData =
+          child.parentData.asOrNull<CustomGraphWidgetParentData>();
+
+      // Cache position of child in the graph.
+      if (parentData != null) {
+        final node = parentData.node;
+        parentData.position = _getPositionForNode(node, graph.allProviders);
+      }
+
+      // Always need to layout the child.
+      child.layout(const BoxConstraints(), parentUsesSize: true);
+
+      // Use the child's size the update the dimensions of the specific row.
+      if (parentData != null) {
+        parentData.size = child.size;
+        sizes[parentData.position!.y][parentData.position!.x] = child.size;
+      }
       child = childAfter(child);
     }
-    size = Size(5000, 5000);
+
+    // Compute the rect for each child
+    final rowHeights = sizes.map((row) {
+      return row.map((size) => size.height).max;
+    }).toList();
+    child = firstChild;
+    while (child != null) {
+      final parentData =
+          child.parentData.asOrNull<CustomGraphWidgetParentData>();
+      if (parentData != null) {
+        final left = sizes[parentData.position!.y]
+                .slice(0, parentData.position!.x)
+                .map((size) => size.width)
+                .sum +
+            parentData.position!.x * _horizontalSpacing;
+        final top = parentData.position!.y * _verticalSpacing +
+            rowHeights.slice(0, parentData.position!.y).sum;
+        parentData.rect = Rect.fromLTWH(
+          _padding.left + left,
+          _padding.top + top,
+          child.size.width,
+          child.size.height,
+        );
+      }
+      child = childAfter(child);
+    }
+
+    // Compute overall parent size.
+    final contentSize = sizes.foldIndexed(Size.zero, (yPos, acc, row) {
+          return Size(
+            max(acc.width, row.map((size) => size.width).sum) +
+                max(0, row.length - 1) * _horizontalSpacing,
+            acc.height + row.map((size) => size.height).max,
+          );
+        }) +
+        (Offset(0, max(0, sizes.length - 1) * _verticalSpacing));
+    size = contentSize + _padding.topLeft + _padding.bottomRight;
   }
 
   @override
   void paint(PaintingContext context, Offset offset) {
-    _cachedChildRects.clear();
-    final allProviders = graph.nodes.map((node) => node.provider).toSet();
     RenderBox? child = firstChild;
+    // Cache rects for all children.
+    Map<GraphNode, Rect> childRects = {};
     while (child != null) {
       final parentData = child.parentData as CustomGraphWidgetParentData;
-      final graphNode = parentData.node;
-      if (graphNode != null) {
-        final offset = _getOffsetForGraphNode(graphNode, allProviders);
-        final rect = Rect.fromCenter(center: offset, width: child.size.width, height: child.size.height);
-        _cachedChildRects[graphNode] = rect;
-      }
+      childRects[parentData.node] = parentData.rect!;
       child = childAfter(child);
     }
+
+    // Paint graph edges.
     final canvas = context.canvas;
     final edgePaint = Paint()
       ..color = Colors.lightBlueAccent
@@ -113,13 +185,13 @@ class RenderCustomGraphWidget extends RenderBox
     for (final edge in _graph.edges) {
       final startNode =
           _graph.nodes.firstWhere((it) => it.provider == edge.from);
-      final endNode =
-          _graph.nodes.firstWhere((it) => it.provider == edge.to);
-      final startOffset = _cachedChildRects[startNode]?.center ?? Offset.zero;
-      final endOffset = _cachedChildRects[endNode]?.center ?? Offset.zero;
+      final endNode = _graph.nodes.firstWhere((it) => it.provider == edge.to);
+      final startOffset = childRects[startNode]?.center ?? Offset.zero;
+      final endOffset = childRects[endNode]?.center ?? Offset.zero;
       final edgeRect = Rect.fromPoints(startOffset, endOffset);
-      final oneNodeIsTopLeft = startOffset == edgeRect.topLeft || endOffset == edgeRect.topLeft;
-      final edgePath = switch(oneNodeIsTopLeft) {
+      final oneNodeIsTopLeft =
+          startOffset == edgeRect.topLeft || endOffset == edgeRect.topLeft;
+      final edgePath = switch (oneNodeIsTopLeft) {
         true => Path()
           ..moveTo(edgeRect.topLeft.dx, edgeRect.topLeft.dy)
           ..quadraticBezierTo(
@@ -151,14 +223,14 @@ class RenderCustomGraphWidget extends RenderBox
       };
       canvas.drawPath(edgePath, edgePaint);
     }
+
+    // Paint children.
     child = firstChild;
     while (child != null) {
       final parentData = child.parentData as CustomGraphWidgetParentData;
       final graphNode = parentData.node;
-      if (graphNode != null) {
-        final offset = _cachedChildRects[graphNode]?.topLeft ?? Offset.zero;
-        context.paintChild(child, offset);
-      }
+      final offset = childRects[graphNode]?.topLeft ?? Offset.zero;
+      context.paintChild(child, offset);
       child = childAfter(child);
     }
   }
